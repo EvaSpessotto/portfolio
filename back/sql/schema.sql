4c46d9ee7d0e1fc3e2836226f8b6a044675b0783
Eva Spessotto <eva@spessotto.com>
2018-12-30 18:21:03 +0100

    Initialisation de la BDD

---
+++ back/sql/schema.sql
@@ -0,0 +1,14 @@
+create table project(
+  id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(100),
+  project_desc TEXT,
+  creation_date date
+);
+
+create table image(
+  title VARCHAR(128),
+  path VARCHAR(250),
+  project_id INTEGER NOT NULL
+);
+
+ALTER TABLE image ADD CONSTRAINT fk_project_id FOREIGN KEY (project_id) REFERENCES project(id);
